9508caa63f5ace32d85b0fe922ef7b7a4e00f7fb
ShengLuo <luos5@uci.edu>
2020-05-20 20:24:38 -0700

    basic functions

--- Insta/models.py
+++ Insta/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.urls import reverse
+from imagekit.models import ProcessedImageField
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+class Post(models.Model):
+    id = models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')
+    title = models.TextField(blank=True, null=True)
+    image = ProcessedImageField(
+        upload_to = 'static/images/posts',
+        format = 'JPEG',
+        options = {'quality':100},
+        blank = True,
+        null = True
+    )
+
+    def get_absolute_url(self):
+        return reverse("post_detail", args = [str(self.id)])
+
+class InstaUser(AbstractUser):
+    profile_pic = ProcessedImageField(
+        upload_to = 'static/images/profiles',
+        format = 'JPEG',
+        options = {'quality':100},
+        blank = True,
+        null = True
+    )
