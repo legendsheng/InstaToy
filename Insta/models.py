f619e68576b8270b847275b02d467d709a38f8b5
ShengLuo <luos5@uci.edu>
2020-05-28 14:27:31 -0700

    fixed post time problem

--- Insta/models.py
+++ Insta/models.py
@@ -90,3 +90,17 @@ class Like(models.Model):
 
     def __str__(self):
         return 'Like: ' + self.user.username + ' likes ' + self.post.title
+
+class Comment(models.Model):
+    post = models.ForeignKey(
+        Post, 
+        on_delete=models.CASCADE, 
+        related_name='comments')
+    user = models.ForeignKey(
+        InstaUser, 
+        on_delete=models.CASCADE)
+    comment = models.CharField(max_length=100)
+    posted_on = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.comment
--- Insta/views.py
+++ Insta/views.py
@@ -2,7 +2,7 @@ from django.views.generic import TemplateView,ListView,DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse, reverse_lazy
 from annoying.decorators import ajax_request
-from Insta.models import Post, Like, UserConnection, InstaUser
+from Insta.models import Post, Like, UserConnection, InstaUser, Comment
 
 from django.contrib.auth.forms import UserCreationForm
 from Insta.form import CustomUserCreationForm, CustomPostCreationForm
@@ -29,6 +29,15 @@ class PostsView(ListView):
 class PostDetailView(DetailView):
     model = Post
     template_name = 'post_detail.html'
+
+    # def get_context_data(self, **kwargs):
+    #     data = super().get_context_data(**kwargs)
+    #     liked = Like.objects.filter(post=self.kwargs.get('pk'), user=self.request.user).first()
+    #     if liked:
+    #         data['liked'] = 1
+    #     else:
+    #         data['liked'] = 0
+    #     return data
 
 class PostCreateView(LoginRequiredMixin, CreateView):
     form_class = CustomPostCreationForm
@@ -112,4 +121,33 @@ def toggleFollow(request):
         'result': result,
         'type': request.POST.get('type'),
         'follow_user_pk': follow_user_pk
+    }
+
+@ajax_request
+def addComment(request):
+    comment_text = request.POST.get('comment_text')
+    post_pk = request.POST.get('post_pk')
+    post = Post.objects.get(pk=post_pk)
+    commenter_info = {}
+
+    try:
+        comment = Comment(comment=comment_text, user=request.user, post=post)
+        comment.save()
+
+        username = request.user.username
+
+        commenter_info = {
+            'username': username,
+            'comment_text': comment_text
+        }
+
+        result = 1
+    except Exception as e:
+        print(e)
+        result = 0
+
+    return {
+        'result': result,
+        'post_pk': post_pk,
+        'commenter_info': commenter_info
     }
