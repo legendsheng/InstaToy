d72fb09bea81ff50800bbf4dca4c6f7a39bd2415
ShengLuo <luos5@uci.edu>
2020-05-28 19:32:14 -0700

    whitenoise4

--- instaDemo/settings.py
+++ instaDemo/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'j9a#!*hndt_lgsk*9okfltlit22eegcj7c@npp5goc20tfu%d9'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
